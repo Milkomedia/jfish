83dcd9f933bdc5625c8bc452414240546ca214e6
strider <kyprus77@gmail.com>
2025-08-10 03:08:51 +0900

    이제 시작이다~

--- src/Controller/controller_geom/include/controller_worker.hpp
+++ src/Controller/controller_geom/include/controller_worker.hpp
@@ -35,7 +35,7 @@ constexpr double mapping_factor_yaw  = CMD_YAW_SPD / 672000.0;
 constexpr double two_PI = 2.0 * M_PI;
 
 constexpr double X_offset = 0.0; // [m] 
-constexpr double Y_offset = 0.6; // [m] 
+constexpr double Y_offset = 0.0; // [m] 
 constexpr double Z_offset = 0.11; // [m] it must be (+) sign.
 
 class ControllerNode : public rclcpp::Node {
